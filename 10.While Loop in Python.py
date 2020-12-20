7c9b6644f20faa57227521dffc8b60ab5e5e82d2
AyushSingh445132 <ayushsingh445132@gamil.com>
2020-12-20 14:10:36 +0530

    "Initial commit"

--- 10.While Loop in Python.py
+++ 10.While Loop in Python.py
@@ -1,5 +1,5 @@
 # While Loop
-var1 = 0
-while var1 < 101:
-    print(var1)
-    var1 = var1 + 1
+i = 0
+while i < 101:
+    print(i)
+    i += 1
---
+++ 8.1Faulty Calculator.py
@@ -0,0 +1,26 @@
+# Making a Faulty Calculator
+print("Welcome to Calculator.Made by Ayush Singh")
+op = input("Please type the mathematical operation you would like to do:\n"
+           "+ for addition\n"
+           "- for subtraction\n"
+           "* for multiplication\n"
+           "/ for division \n")
+num1 = int(input("Enter First Number\n"))
+num2 = int(input("Enter Second Number\n"))
+
+if op=="+" and num1==56 and num2==9:
+     print (77)
+elif op=="+":
+    print(num1 + num2)
+elif op=="-":
+    print(num1 - num2)
+elif op=="*" and num1==45 and num2==3:
+        print(555)
+elif op == "*":
+    print(num1 * num2)
+elif op=="/" and num1==56 and num2==6:
+        print(6)
+elif op=="/":
+    print(num1 / num2)
+else :
+    print("Unexpected Error!!Please check your equation again.")
