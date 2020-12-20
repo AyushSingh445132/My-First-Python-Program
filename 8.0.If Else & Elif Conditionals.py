0727014884e42e83c004c92426c6cb42517d559d
AyushSingh445132 <ayushsingh445132@gamil.com>
2020-12-20 19:25:49 +0530

    "Initial commit"

--- 8.0.If Else & Elif Conditionals.py
+++ 8.0.If Else & Elif Conditionals.py
@@ -2,13 +2,13 @@
 print ("What's the temperature Outside(in Celsius)?")
 T1 = int(input())
 if T1>35:
-    print("Wear Cotton Clothes,don't stay outside in the sun for too long.")
+    print("Wear Cotton Clothes,don't stay outside in the sun for too long or you will be become a fried chicken")
 elif T1>20<35:
-   print("Go Outside and Enjoy the wheather.")
+   print("Go Outside and Enjoy the wheather.Why area you siting at home and doing nothing")
 elif T1>10<20:
    print("It might be cold outside.Get some Exersize done to feel better or you will be a lazy sloth.")
 elif T1<10:
-    print("Try keeping your body warm and Be Aware not to catch a cold.")
+    print("Try keeping your body warm and Be Aware not to catch a cold or you will become a coughing Snowman.")
 elif T1> 50:
     print("You probably live in a desert or in a volcano.")
 elif T1< 0:
