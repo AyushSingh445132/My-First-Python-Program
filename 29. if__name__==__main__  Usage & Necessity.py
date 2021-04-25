d4d2b9151ae585763c8e8c4e2808ca91deb4d8f5
AyushSingh445132 <ayushsingh445132@gamil.com>
2021-04-25 19:31:48 +0530

    Initial commit

---
+++ 29. if__name__==__main__  Usage & Necessity.py
@@ -0,0 +1,8 @@
+# A __name__ is a built-in variable that returns us the name of the module being used.
+
+import file1
+
+file1.average(5, 25)
+
+print("__name__ in file1.py is a set to " +__name__)
+
---
+++ file1.py
@@ -0,0 +1,10 @@
+def average(a, b):
+    sum = a + b
+    print(sum/2)
+
+# print("__name__ in file1.py is a set to " +__name__)
+print("__main__")
+if __name__=="__main__":
+    average(25, 35)
+
+
